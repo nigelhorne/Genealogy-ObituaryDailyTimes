6e74ef87ef85d3d70c83cca2596bc367c31b4c88
Nigel Horne <njh@bandsman.co.uk>
2023-02-24 18:03:58 -0500

    Release 0.08

--- lib/Genealogy/ObituaryDailyTimes.pm
+++ lib/Genealogy/ObituaryDailyTimes.pm
@@ -14,11 +14,11 @@ Genealogy::ObituaryDailyTimes - Lookup an entry in the Obituary Daily Times
 
 =head1 VERSION
 
-Version 0.07
+Version 0.08
 
 =cut
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 =head1 SYNOPSIS
 
