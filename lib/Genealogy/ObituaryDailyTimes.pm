7a13fb90bd95e8230f2460edd67ff58e2819cf85
Nigel Horne <njh@bandsman.co.uk>
2024-02-06 14:36:53 -0500

    Fix AUTOMATED_TESTING

--- lib/Genealogy/ObituaryDailyTimes.pm
+++ lib/Genealogy/ObituaryDailyTimes.pm
@@ -176,7 +176,7 @@ L<http://deps.cpantesters.org/?module=Genealogy::ObituaryDailyTimes>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2020-2023 Nigel Horne.
+Copyright 2020-2024 Nigel Horne.
 
 This program is released under the following licence: GPL2
 
