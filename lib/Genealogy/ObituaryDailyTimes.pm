b56fc522f9cf3854fddfb083128451e0e1c491d1
Nigel Horne <njh@bandsman.co.uk>
2024-12-10 15:46:52 -0500

    ensure that the 'newspaper' key in the  hash is defined when the source is 'L'

--- lib/Genealogy/ObituaryDailyTimes.pm
+++ lib/Genealogy/ObituaryDailyTimes.pm
@@ -7,6 +7,12 @@ use File::Spec;
 use Module::Info;
 use Genealogy::ObituaryDailyTimes::obituaries;
 
+use constant URLS => {
+	# 'M' => "https://mlarchives.rootsweb.com/listindexes/emails?listname=gen-obit&page=",
+	'M' => "https://wayback.archive-it.org/20669/20231102044925/https://mlarchives.rootsweb.com/listindexes/emails?listname=gen-obit&page=",
+	'F' => "https://www.freelists.org/post/obitdailytimes/Obituary-Daily-Times-",
+};
+
 =head1 NAME
 
 Genealogy::ObituaryDailyTimes - Lookup an entry in the Obituary Daily Times
@@ -128,16 +134,16 @@ sub _create_url {
 	}
 
 	if($source eq 'M') {
-		# return "https://mlarchives.rootsweb.com/listindexes/emails?listname=gen-obit&page=$page";
-		return "https://wayback.archive-it.org/20669/20231102044925/https://mlarchives.rootsweb.com/listindexes/emails?listname=gen-obit&page=$page";
+		return URLS->{'M'} . $page;
 	}
 	if($source eq 'F') {
-		return "https://www.freelists.org/post/obitdailytimes/Obituary-Daily-Times-$page";
+		return URLS->{'F'} . $page;
 	}
 	if($source eq 'L') {
-		return $obit->{'newspaper'};
+		my $newspaper = $obit->{'newspaper'} || Carp::croak(__PACKAGE__, ": undefined newspaper.  Newspaper much be given when source type is 'L'");
+		return $newspaper;
 	}
-	Carp::croak(__PACKAGE__, ": Invalid source, '$source'");
+	Carp::croak(__PACKAGE__, ": Invalid source, '$source'. Valid sources are 'M', 'F' and 'L'");
 }
 
 # Helper routine to parse the arguments given to a function,
